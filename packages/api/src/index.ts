5a298811d771615b699a21167c379090c1467887
Joe Buza <joebm08@gmail.com>
2020-03-17 23:08:15 -0700

    Added mutation and trivial resolver

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,15 +1,44 @@
 import { ApolloServer, IResolvers } from 'apollo-server'
 
+interface Photo {
+    name: String
+    description?: String
+}
+
+const photos: Photo[] = []
 const typeDefs: string = `
+    type Photo {
+        id: ID!
+        url: String!
+        name: String!
+        description: String
+    }
     type Query {
         totalPhotos: Int!
+        getPhotos: [Photo!]!
+    }
+
+    type Mutation {
+        addPhoto(name: String! description: String): Photo
     }
 `
 
+
 const resolvers: IResolvers = {
     Query: {
-        totalPhotos: () => 40,
+        totalPhotos: () => photos.length,
+        getPhotos: () => photos
     },
+    Mutation: {
+        addPhoto: (_, args) => {
+            const newPhoto = { ...args, id: photos.length}
+            photos.push(newPhoto)
+            return newPhoto
+        }
+    },
+    Photo: {
+        url: parent => `https://i.picsum.photos/id/911/200/30${parent.id}.jpg`
+    }
 }
 
 const server = new ApolloServer({
