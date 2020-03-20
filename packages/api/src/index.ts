3f90075645224fa3f0948ee1f5d8b76e7ff64be9
Joe Buza <joebm08@gmail.com>
2020-03-19 20:17:18 -0700

    one to many

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,25 +1,95 @@
 import { ApolloServer, IResolvers } from 'apollo-server'
 
-interface Photo {
-    name: String
-    description?: String
+enum PhotoCategory {
+    SELFIE = 'SELFIE',
+    POTRAIT = 'POTRAIT',
+    ACTION = 'ACTION',
+    LANDSCAPE = 'LANDSCAPE',
+    GRAPHIC = 'GRAPHIC',
 }
 
-const photos: Photo[] = []
+interface User {
+    githubLogin: String
+    name?: String
+    postedPhotos?: Photo[]
+}
+interface Photo {
+    id: String
+    name: String
+    description?: String
+    url: String
+    category: PhotoCategory
+    githubUser: String
+}
+
+const users: User[] = [
+    { githubLogin: 'joe', name: 'Joe Buza' },
+    { githubLogin: 'alan', name: 'Alan Souza' },
+]
+const photos: Photo[] = [
+    {
+        "id": "0",
+        "url": "https://i.picsum.photos/id/911/200/300.jpg",
+        "name": "Joe",
+        "description": "Buza",
+        "category": PhotoCategory.POTRAIT,
+        githubUser: users[0].githubLogin
+    },
+    {
+        "id": "1",
+        "url": "https://i.picsum.photos/id/911/200/301.jpg",
+        "name": "Joe",
+        "description": "Buza",
+        "category": PhotoCategory.POTRAIT,
+        githubUser: users[1].githubLogin
+    }
+]
 const typeDefs: string = `
+    enum PhotoCategory {
+        SELFIE
+        POTRAIT
+        ACTION
+        LANDSCAPE
+        GRAPHIC
+    }
+
+    input PostPhotoInput {
+        name: String!
+        description: String
+        category: PhotoCategory=POTRAIT
+        githubUser: String!
+    }
+
+    input UserInput {
+        githubLogin: String!
+        name: String
+    }
+
     type Photo {
         id: ID!
         url: String!
         name: String!
         description: String
+        category: PhotoCategory!
+        postedBy: User!
+    }
+
+    type User {
+        githubLogin: ID!
+        name: String
+        avatar: String
+        postedPhotos: [Photo!]!
     }
     type Query {
         totalPhotos: Int!
         getPhotos: [Photo!]!
+        getUsers: [User!]!
+        getUser(input: UserInput!): User
     }
 
     type Mutation {
-        addPhoto(name: String! description: String): Photo
+        addPhoto(input: PostPhotoInput!): Photo!
+        addUser(input: UserInput!): User!
     }
 `
 
@@ -27,18 +97,30 @@ const typeDefs: string = `
 const resolvers: IResolvers = {
     Query: {
         totalPhotos: () => photos.length,
-        getPhotos: () => photos
+        getPhotos: () => photos,
+        getUsers: () => users,
+        getUser: (_, { input }) => users.find(({ githubLogin }) => githubLogin === input.githubLogin)
     },
     Mutation: {
-        addPhoto: (_, args) => {
-            const newPhoto = { ...args, id: photos.length}
+        addPhoto: (_, { input }) => {
+            const newPhoto = { ...input, id: photos.length }
             photos.push(newPhoto)
             return newPhoto
+        },
+        addUser: (_, { input }) => {
+            const newUser = { ...input }
+            users.push(newUser)
+            return newUser
         }
     },
     Photo: {
-        url: parent => `https://i.picsum.photos/id/911/200/30${parent.id}.jpg`
-    }
+        url: parent => `https://i.picsum.photos/id/911/200/30${parent.id}.jpg`,
+        postedBy: parent => users.find(({githubLogin}) => parent.githubUser === githubLogin)
+    },
+    User: {
+        avatar: parent => `https://i.picsum.photos/id/911/200/30${parent.githubLogin}.jpg`,
+        postedPhotos: parent => photos.filter(({ githubUser }) => githubUser === parent.githubLogin)
+    },
 }
 
 const server = new ApolloServer({
